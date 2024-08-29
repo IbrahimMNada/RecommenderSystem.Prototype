93aaf7b8ec4b54907d3798d41bfbdbab0d757544
inada <inada@2p.com.sa>
2024-08-29 10:13:21 +0300

    refactor

--- SqlServerManager.py
+++ SqlServerManager.py
@@ -6,74 +6,44 @@ class DatabaseReader:
     def __init__(self, db_host,db_name, db_user,db_password):
         self.connection_string = 'Driver={SQL Server};Server=' + db_host + ';Database=' + db_name + ';UID=' + db_user + ';PWD=' + db_password + ';'
     
+    def _executeQuery(self,query,param=None):
+        db = pyodbc.connect(self.connection_string)
+        cursor = db.cursor()
+        cursor.execute(query,param)
+        query_result = (cursor.description ,cursor.fetchall())
+        cursor.close()
+        db.close()
+        return query_result
+    
+    def _resultToDict(self,query_result):
+        final_result = [dict(zip([column[0] for column in query_result[0]], row)) for row in query_result[1]]
+        return final_result
 
     def GetAllUsers(self):
-        db = pyodbc.connect(self.connection_string)
-        SQL = ' select id as user_id , * from Users '
-        cursor = db.cursor()
-        cursor.execute(SQL)
-        query_results = [dict(zip([column[0] for column in cursor.description], row)) for row in cursor.fetchall()]
-        cursor.close()
-        db.close()
-        return query_results
+        queryResult = self._executeQuery(' select * from Users ')
+        return self._resultToDict(queryResult)
               
     def GetAllPosts(self):
-        db = pyodbc.connect(self.connection_string)
-        SQL = ' select Id as post_id , title  from Posts '
-        cursor = db.cursor()
-        cursor.execute(SQL)
-        query_results = [dict(zip([column[0] for column in cursor.description], row)) for row in cursor.fetchall()]
-        cursor.close()
-        db.close()
-        return query_results
+        queryResult = self._executeQuery(' select Id as post_id , title  from Posts ')
+        return self._resultToDict(queryResult)
     
     def GetAllPostViews(self):
-        db = pyodbc.connect(self.connection_string)
-        SQL = ' select userid as [user_id] , Postid as post_id , intrest   from [dbo].[PostViews] '
-        cursor = db.cursor()
-        cursor.execute(SQL)
-        query_results = [dict(zip([column[0] for column in cursor.description], row)) for row in cursor.fetchall()]
-        cursor.close()
-        db.close()
-        return query_results
+        queryResult = self._executeQuery(' select userid as [user_id] , Postid as post_id , intrest   from [dbo].[PostViews] ')
+        return self._resultToDict(queryResult)
   
     def GetPostCategory(self,postId):
-        db = pyodbc.connect(self.connection_string)
-        SQL = ' select Category from posts where id =  ' + str(postId)
-        cursor = db.cursor()
-        cursor.execute(SQL)
-        query_results = cursor.fetchone()
-        cursor.close()
-        db.close()
-        return query_results
+        queryResult = self._executeQuery(' select Category from posts where id = ? ' , (postId,))
+        return self._resultToDict(queryResult)
     
     def GetIntrestsWithPosts(self):
-        db = pyodbc.connect(self.connection_string)
-        SQL = ' select * from [Posts] p join PostViews pv on p.id = pv.postId '
-        cursor = db.cursor()
-        cursor.execute(SQL)
-        query_results = [dict(zip([column[0] for column in cursor.description], row)) for row in cursor.fetchall()]
-        cursor.close()
-        db.close()
-        return query_results
+        queryResult = self._executeQuery(' select * from [Posts] p join PostViews pv on p.id = pv.postId ')
+        return self._resultToDict(queryResult)
 
     
     def GetRateingLimits(self):
-        db = pyodbc.connect(self.connection_string)
-        SQL = ' select min(intrest) As MinIntrest  , max(intrest) As MaxIntrest  from PostViews '
-        cursor = db.cursor()
-        cursor.execute(SQL)
-        query_results = cursor.fetchone()
-        cursor.close()
-        db.close()
-        return query_results
+        queryResult = self._executeQuery(' select min(intrest) As MinIntrest  , max(intrest) As MaxIntrest  from PostViews ')
+        return self._resultToDict(queryResult)[0]
     
     def GetPostsNotViewedByUser(self,userId):
-        db = pyodbc.connect(self.connection_string)
-        SQL = ' select * from [Posts] p join PostViews pv on p.id = pv.postId where Userid <>  ? '
-        cursor = db.cursor()
-        cursor.execute(SQL,(userId,))
-        query_results = [dict(zip([column[0] for column in cursor.description], row)) for row in cursor.fetchall()]
-        cursor.close()
-        db.close()
-        return query_results
+        queryResult = self._executeQuery(' select * from [Posts] p join PostViews pv on p.id = pv.postId where Userid <>  ?' , (userId,))
+        return self._resultToDict(queryResult)
